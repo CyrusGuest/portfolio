cb96d53e7618ee640fb445cc67594815285e5f37
Cyrus Guest <cyrus.t.guest@gmail.com>
2023-01-12 17:47:49 -0500

    updates

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,32 +1,103 @@
-import React from 'react'
-import styles from './projects.module.css'
+import React from "react";
+import styles from "./projects.module.css";
 
-const ProjectCard = ({ contentSide, title, description, img, color, url }) => {
-  if (contentSide === "left") return (
-    <div className={styles.projectCardContainer} style={{ backgroundColor: color }}>
-      <div className={styles.projectCardContentContainer}>
-        <h3 className={styles.projectCardTitle}>{title}</h3>
-        <p className={styles.projectCardDescription}>{description}</p>
-        <a href={url} target="_blank" rel="noreferrer" className={styles.projectCardBtn} style={{ backgroundColor: color }}>Visit Project</a>
+const ProjectCard = ({
+  contentSide,
+  title,
+  description,
+  img,
+  color,
+  repoUrl,
+  url,
+  live,
+}) => {
+  if (contentSide === "left")
+    return (
+      <div
+        className={styles.projectCardContainer}
+        style={{ backgroundColor: color }}
+      >
+        <div className={styles.projectCardContentContainer}>
+          <h3 className={styles.projectCardTitle}>{title}</h3>
+          <p className={styles.projectCardDescription}>{description}</p>
+          <div className={styles.buttonContainer}>
+            <a
+              href={repoUrl}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.projectCardBtn}
+              style={{ backgroundColor: color }}
+            >
+              Visit Repo
+            </a>
+            {live ? (
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.projectCardBtn}
+                style={{ backgroundColor: color }}
+              >
+                Visit Site
+              </a>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+        <div className={styles.projectCardImgContainer}>
+          <img className={styles.projectCardImg} src={img} alt="" />
+        </div>
       </div>
-      <div className={styles.projectCardImgContainer}>
-        <img className={styles.projectCardImg} src={img} alt="" />
-      </div>
-    </div>
-  )
+    );
 
-  if (contentSide === "right") return (
-    <div className={styles.projectCardContainer} style={{ backgroundColor: color }}>
-      <div className={styles.projectCardImgContainer}>
-        <img className={styles.projectCardImg} src={img} alt="" />
+  if (contentSide === "right")
+    return (
+      <div
+        className={styles.projectCardContainer}
+        style={{ backgroundColor: color }}
+      >
+        <div className={styles.projectCardImgContainer}>
+          <img className={styles.projectCardImg} src={img} alt="" />
+        </div>
+        <div
+          style={{
+            borderTopRightRadius: "15px",
+            borderBottomRightRadius: "15px",
+            borderTopLeftRadius: "0",
+            borderBottomLeftRadius: "0",
+          }}
+          className={styles.projectCardContentContainer}
+        >
+          <h3 className={styles.projectCardTitle}>{title}</h3>
+          <p className={styles.projectCardDescription}>{description}</p>
+          <div className={styles.buttonContainer}>
+            <a
+              href={repoUrl}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.projectCardBtn}
+              style={{ backgroundColor: color }}
+            >
+              Visit Repo
+            </a>
+            {live ? (
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.projectCardBtn}
+                style={{ backgroundColor: color }}
+              >
+                Visit Site
+              </a>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
       </div>
-      <div style={{ borderTopRightRadius: "15px", borderBottomRightRadius: "15px", borderTopLeftRadius: "0", borderBottomLeftRadius: "0",  }} className={styles.projectCardContentContainer}>
-        <h3 className={styles.projectCardTitle}>{title}</h3>
-        <p className={styles.projectCardDescription}>{description}</p>
-        <a href={url} target="_blank" rel="noreferrer" className={styles.projectCardBtn} style={{ backgroundColor: color }}>Visit Project</a>
-      </div>
-  </div>
-  )
-}
+    );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import styles from './projects.module.css'
-import ProjectCard from './ProjectCard';
-import puppeteerLogo from '../../images/puppeteer.png';
-import cssLogo from '../../images/css3.png';
-import reactLogo from '../../images/react.png';
+import React from "react";
+import styles from "./projects.module.css";
+import ProjectCard from "./ProjectCard";
+import puppeteerLogo from "../../images/puppeteer.png";
+import jadedprintsLogo from "../../images/jadedprints.webp";
+import SADDLogo from "../../images/SADD.png";
 
 const Projects = () => {
   return (
     <div className={styles.projectSectionContainer} id="projects">
       <h1 className={styles.title}>Projects</h1>
       <div className={styles.projectCardsContainer}>
-        <ProjectCard url="https://github.com/CyrusFPS/amazon-bot" contentSide="left" title="Amazon bot with Puppeteer" img={puppeteerLogo} color="#8188A3" description="This was a small project I started out of curiosity, and morphed into something with real world use. The goal was to create an app that could help users track product prices and automatically purchase them." />
-        <ProjectCard url="https://github.com/CyrusFPS/task-manager" contentSide="right" title="Basic task manager" img={cssLogo} color="#85BDFF" description="The basic task manager was a small project that I created to help me learn and fundamentally understand CSS grid. It helped progress my knowledge of CSS and helped grow me into the developer I am today." />
-        <ProjectCard url="https://github.com/CyrusFPS/grocerylist-app" contentSide="left" title="Family grocery list app" img={reactLogo} color="#424242" description="I began developement on this app to learn React Native. I haven’t finished it, and might not ever, but it was an amazing introduction into mobile app developement. I certainly plan to do more React Native developement." />
+        <ProjectCard
+          repoUrl="https://github.com/CyrusGuest/jadedprints/"
+          url="https://jadedprints.com"
+          contentSide="left"
+          title="Ecommerce store w/ analytics"
+          img={jadedprintsLogo}
+          color="#443f47"
+          description="This is the largest project I've ever made. It's an ecommerce store that uses stripe to process payments. Google analytics is integrated aswell to track data such as conversion rates. The design, code, and products were all made by me. Technologies used include - Stripe, ReactJS, TailwindCSS, DigitalOcean, NodeJS, Express, Figma, Axios, and more."
+          live={true}
+        />
+        <ProjectCard
+          repoUrl="https://github.com/CyrusGuest/SADD-website"
+          url="https://hopedalesadd.org"
+          contentSide="right"
+          title="SADD Website"
+          img={SADDLogo}
+          color="#5167df"
+          description="After spending a year with my local SADD (Students Against Destructive Decisions) chapter in school, I thought that I could greatly aid them in their goals by providing a website. Made withr ReactJS, TailwindCSS, Figma, and more, the website helped me learn aswell as aided the community. The SADD chapter still exists on without me at Northbridge Highschool."
+          live={true}
+        />
+        <ProjectCard
+          repoUrl="https://github.com/CyrusFPS/doctordetector"
+          contentSide="left"
+          title="Primary Care Doctor Search"
+          img={puppeteerLogo}
+          color="#424242"
+          description="My dad texted me one day saying he had a real-world solution that he wanted me to solve. He couldn't get a primary care doctor. So, I developed a web-scraping bot that checks his medical-group's search for any primary care doctors accepting new patients. When one is found, it emails him and he'll be the first one to call to get the doctor."
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
